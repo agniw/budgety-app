3fc7f6629066aa0615aeda56cb178d43b82cf151
Agnieszka Wojsznis <agnieszka.wojsznis@gmail.com>
2018-09-29 00:00:20 +0200

    add input to data structure

--- app.js
+++ app.js
@@ -1,5 +1,57 @@
 var budgetController = (function() {
 
+    var Income = function(id, description, value) {
+
+        this.id = id;
+        this.description = description;
+        this.value = value;
+
+    };
+
+    var Expense = function(id, description, value) {
+
+        this.id = id;
+        this.description = description;
+        this.value = value;
+
+    };
+
+
+    var data = {
+        allItems: {
+          exp: [],
+          inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        //allows add item to data structure
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val)
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val)
+            }
+
+            data.allItems[type].push(newItem);
+
+            return newItem;
+        }
+    }
+
 })();
 
 
@@ -32,24 +84,41 @@ var UIController = (function() {
 
 var appController = (function(ctrlBudget, ctrlUI) {
 
-    var DOM = ctrlUI.getDOMStrings();
+    var setupEventListeners = function() {
+
+        var DOM = ctrlUI.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener ('keypress', function (e) {
+            if (e.code ===13 || e.keyCode === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
 
     var ctrlAddItem = function() {
+        var input, newItem;
+
         //1. get input field from user
-        var input = UIController.getInput();
+        input = ctrlUI.getInput();
+
         //2. add input do budgetController
+        newItem = ctrlBudget.addItem(input.type, input.description, input.value);
+
         //3. add item to UI
         //4. calculate budget
         //5. update budget
     };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener ('keypress', function (e) {
-        if (e.code ===13 || e.keyCode === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('project started');
+            setupEventListeners();
         }
-    })
+    }
 
 })(budgetController, UIController);
+
+appController.init();
 
